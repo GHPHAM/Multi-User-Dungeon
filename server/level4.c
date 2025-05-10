4f7a4d2f3613f7fa1760e6e0c977fa491c10e3ff
Hoover <reallegofan@gmail.com>
2025-05-09 18:47:56 -0700

    level stitching init

---
+++ server/level4.c
@@ -0,0 +1,81 @@
+#include "dungeon.h"
+// PLACEHOLDER
+
+// External reference to the level array defined in main.c
+extern Level level[4];
+
+void initLevel1() // North, South, East, West
+{
+    level[3].room[9] = (Node){
+        NULL,
+        NULL,
+        NULL,
+        &level[3].room[8],
+        END, strdup("Room 9")};
+
+    level[3].room[8] = (Node){
+        NULL,
+        NULL,
+        &level[3].room[9],
+        &level[3].room[3],
+        EMPTY, strdup("Room 8")};
+
+    level[3].room[7] = (Node){
+        &level[3].room[5],
+        NULL,
+        NULL,
+        NULL,
+        EMPTY, strdup("Room 7")};
+
+    level[3].room[6] = (Node){
+        NULL,
+        NULL,
+        &level[3].room[6],
+        NULL,
+        EMPTY, strdup("Room 6")};
+
+    level[3].room[5] = (Node){
+        &level[3].room[4],
+        &level[3].room[7],
+        &level[3].room[6],
+        NULL,
+        HAS_ITEM, strdup("Room 5")};
+
+    level[3].room[4] = (Node){
+        NULL,
+        &level[3].room[5],
+        NULL,
+        &level[3].room[1],
+        EMPTY, strdup("Room 4")};
+
+    level[3].room[3] = (Node){
+        NULL,
+        NULL,
+        &level[3].room[8],
+        &level[3].room[2],
+        EMPTY, strdup("Room 3")};
+
+    level[3].room[2] = (Node){
+        NULL,
+        &level[3].room[1],
+        &level[3].room[3],
+        NULL,
+        EMPTY, strdup("Room 2")};
+
+    level[3].room[1] = (Node){
+        &level[3].room[2],
+        NULL,
+        &level[3].room[4],
+        &level[3].room[0],
+        EMPTY, strdup("Room 1")};
+
+    level[3].room[0] = (Node){
+        NULL,
+        NULL,
+        &level[3].room[1],
+        NULL,
+        START, strdup("Room 0")};
+
+    level[3].startNode = &level[3].room[0];
+    level[3].endNode   = &level[3].room[9];
+}
