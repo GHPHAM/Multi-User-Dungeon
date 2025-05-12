4e1ccd2584bbc6b10d549c74a322115450abe97c
HungHoPomona <hunganhho@cpp.edu>
2025-05-12 03:18:07 -0700

    Update input validation, fix bug on item
    Update input validation for continue or not, fix bug on item when first encounter

--- server/main.c
+++ server/main.c
@@ -23,34 +23,41 @@ void initLevels()
 }
 
 // Display room information and available exits
-void displayRoom(Node* currentRoom) {
+void displayRoom(Node *currentRoom)
+{
     printf("\n=== %s ===\n", currentRoom->desc);
 
     printf("Exits: ");
-    if (currentRoom->north) printf("North ");
-    if (currentRoom->south) printf("South ");
-    if (currentRoom->east) printf("East ");
-    if (currentRoom->west) printf("West ");
+    if (currentRoom->north)
+        printf("North ");
+    if (currentRoom->south)
+        printf("South ");
+    if (currentRoom->east)
+        printf("East ");
+    if (currentRoom->west)
+        printf("West ");
     printf("\n");
 
     // Display special room attributes
-    switch(currentRoom->attribute) {
-        case START:
-            printf("This is the starting point.\n");
-            break;
-        case END:
-            printf("This is the exit of the dungeon!\n");
-            break;
-        case HAS_ITEM:
-            printf("There's something interesting here.\n");
-            break;
-        default:
-            break;
+    switch (currentRoom->attribute)
+    {
+    case START:
+        printf("This is the starting point.\n");
+        break;
+    case END:
+        printf("This is the exit of the dungeon!\n");
+        break;
+    case HAS_ITEM:
+        printf("There's something interesting here.\n");
+        break;
+    default:
+        break;
     }
 }
 
 // Get user input for movement
-char getCommand() {
+char getCommand()
+{
     char command;
     printf("\nWhere would you like to go? (w/a/s/d/q): ");
     scanf(" %c", &command);
@@ -58,35 +65,41 @@ char getCommand() {
 }
 
 // Free allocated memory
-void cleanupLevel() {
+void cleanupLevel()
+{
     for (int i = 0; i < sizeof(level); ++i)
     {
-        for (int j = 0; j < 10; ++j) {
+        for (int j = 0; j < 10; ++j)
+        {
             free(level->room[i].desc);
         }
     }
 }
 
 // shuffling the number
-void shuffle(short int *array, int size) {
+void shuffle(short int *array, int size)
+{
     srand(time(NULL));
-    for (int i = size - 1; i > 0; --i) {
+    for (int i = size - 1; i > 0; --i)
+    {
         int j = rand() % (i + 1);
         int temp = array[i];
         array[i] = array[j];
         array[j] = temp;
     }
 
-    //debug
+    // debug
     printf("Run seed: ");
-    for (int i = 0; i < size; ++i){
-        printf("%d",array[i]);
+    for (int i = 0; i < size; ++i)
+    {
+        printf("%d", array[i]);
     }
     printf("\n");
 }
 
-int main() {
-    Node* currentRoom;
+int main()
+{
+    Node *currentRoom;
     char command;
     int playing = 1;
 
@@ -102,72 +115,107 @@ int main() {
     shuffle(levelOrder, size);
 
     // Joining levels
-    for (int i = 0; i < size-1; ++i)
+    for (int i = 0; i < size - 1; ++i)
     {
-        level[levelOrder[i]].room[9].east = &level[levelOrder[i+1]].room[0];
-        level[levelOrder[i+1]].room[0].west = &level[levelOrder[i]].room[9];
+        level[levelOrder[i]].room[9].east = &level[levelOrder[i + 1]].room[0];
+        level[levelOrder[i + 1]].room[0].west = &level[levelOrder[i]].room[9];
     }
 
     // Start at the beginning
     currentRoom = level[levelOrder[0]].startNode;
 
     // Main game loop
-    while (playing) {
+    while (playing)
+    {
         displayRoom(currentRoom);
 
         // Check if we're at the end
-        if (currentRoom->attribute == HAS_ITEM) {
+        if (currentRoom->attribute == HAS_ITEM)
+        {
             printf("\nCongratulations! You have found the item!\n");
-            printf("Would you like to continue exploring? (y/n): ");
-            scanf(" %c", &command);
-            if (tolower(command) != 'y')
-                break;
+            currentRoom->attribute = EMPTY; // Mark the item as found so when continuing exploring should not be found again
+
+            do // Input validation
+            {
+                printf("Would you like to continue exploring? (y/n): ");
+                command = getchar();
+                while (getchar() != '\n')
+                    ; // clear extra buffer
+                if (tolower(command) == 'y')
+                    break;
+
+                else if (tolower(command) == 'n')
+                {
+                    printf("\nThank you for playing\n");
+                    cleanupLevel();
+                    exit(0);
+                }
+
+                else
+                {
+                    printf("\nInvalid input. Please enter 'y' or 'n'.\n");
+                }
+
+            } while (1);
         }
 
         // Get command
         command = getCommand();
 
         // Process command
-        switch(command) {
-            case 'w':
-                if (currentRoom->north) {
-                    currentRoom = currentRoom->north;
-                    printf("\nYou move north.\n");
-                } else {
-                    printf("\nYou can't go that way.\n");
-                }
-                break;
-            case 's':
-                if (currentRoom->south) {
-                    currentRoom = currentRoom->south;
-                    printf("\nYou move south.\n");
-                } else {
-                    printf("\nYou can't go that way.\n");
-                }
-                break;
-            case 'd':
-                if (currentRoom->east) {
-                    currentRoom = currentRoom->east;
-                    printf("\nYou move east.\n");
-                } else {
-                    printf("\nYou can't go that way.\n");
-                }
-                break;
-            case 'a':
-                if (currentRoom->west) {
-                    currentRoom = currentRoom->west;
-                    printf("\nYou move west.\n");
-                } else {
-                    printf("\nYou can't go that way.\n");
-                }
-                break;
-            case 'q':
-                printf("\nThanks for playing!\n");
-                playing = 0;
-                break;
-            default:
-                printf("\nInvalid command. Use w, a, s, d, or q.\n");
-                break;
+        switch (command)
+        {
+        case 'w':
+            if (currentRoom->north)
+            {
+                currentRoom = currentRoom->north;
+                printf("\nYou move north.\n");
+            }
+            else
+            {
+                printf("\nYou can't go that way.\n");
+            }
+            break;
+        case 's':
+            if (currentRoom->south)
+            {
+                currentRoom = currentRoom->south;
+                printf("\nYou move south.\n");
+            }
+            else
+            {
+                printf("\nYou can't go that way.\n");
+            }
+            break;
+        case 'd':
+            if (currentRoom->east)
+            {
+                currentRoom = currentRoom->east;
+                printf("\nYou move east.\n");
+            }
+            else
+            {
+                printf("\nYou can't go that way.\n");
+            }
+            break;
+        case 'a':
+            if (currentRoom->west)
+            {
+                currentRoom = currentRoom->west;
+                printf("\nYou move west.\n");
+            }
+            else
+            {
+                printf("\nYou can't go that way.\n");
+            }
+            break;
+        case 'q':
+            printf("\nThanks for playing!\n");
+            playing = 0;
+            break;
+        default:
+            printf("\nInvalid command. Use w, a, s, d, or q.\n");
+            break;
         }
     }
 
