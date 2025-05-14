be6359b58e6a4db0b8e5805d83474bbb4f9a331f
BEaNs-Toast <bmtzobery@cpp.edu>
2025-05-13 21:07:12 -0700

    Update level1.c
    Fixed all the typos

--- server/level1.c
+++ server/level1.c
@@ -15,28 +15,28 @@ void initLevel1() // North, South, East, West
         &level[0].room[0],
         &level[0].room[7],
         NULL,
-        END, strdup("You reach the throne room with endless riches. /nCongradualtions!!!/n you can choose to go south to end up in the starting room, or you can explore the other door")};
+        END, strdup("You reach the throne room with endless riches. \nCongradualtions!!!/n you can choose to go south to end up in the starting room, or you can explore the other door")};
 
     level[0].room[8] = (Node){
         NULL,
         &level[0].room[0],
         NULL,
         NULL,
-        HAS_ITEM, strdup("You find a chest, which you open find an item. /nUnfortualnty, there is no exit except a hole that seems to take you back to the starting room.")};
+        HAS_ITEM, strdup("You find a chest, which you open find an item. \nUnfortualnty, there is no exit except a hole that seems to take you back to the starting room.")};
 
     level[0].room[7] = (Node){
         &level[0].room[6],
         NULL,
         &level[0].room[8],
         &level[0].room[9],
-        EMPTY, strdup("You reach the top of the maze, where you see two doors, one in the east and one in the west. /n You spy something shiny towards the east door")};
+        EMPTY, strdup("You reach the top of the maze, where you see two doors, one in the east and one in the west. \nYou spy something shiny towards the east door")};
 
     level[0].room[6] = (Node){
         NULL,
         &level[0].room[7],
         NULL,
         &level[0].room[5],
-        EMPTY, strdup("Your just out of breath as you see.....\n ....even more stairs (last one I promise).")};
+        EMPTY, strdup("Your just out of breath as you see.....\n....even more stairs (last one I promise).")};
 
     level[0].room[5] = (Node){
         NULL,
@@ -50,14 +50,14 @@ void initLevel1() // North, South, East, West
         NULL,
         &level[0].room[3],
         NULL,
-        EMPTY, strdup("You see more stairs as you enter the room. /nYou start to wonder why did they just not add an elevator to the maze. ")};
+        EMPTY, strdup("You see more stairs as you enter the room. \nYou start to wonder why did they just not add an elevator to the maze. ")};
 
     level[0].room[3] = (Node){
         &level[0].room[2],
         NULL,
         NULL,
         &level[0].room[4],
-        EMPTY, strdup("Guess whats at the top of the stairs?/n If you guess a straigth corridor, you would be incorect.")};
+        EMPTY, strdup("Guess whats at the top of the stairs?\n If you guess a straigth corridor, you would be incorect.")};
 
     level[0].room[2] = (Node){
         NULL,
